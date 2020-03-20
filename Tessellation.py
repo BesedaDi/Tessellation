25695937d468b347a5c17426b4febcb0d7055cfc
BesedaDi <dianabesedina223@gmail.com>
2020-03-20 16:30:39 +0700

    start

--- Tessellation.py
+++ Tessellation.py
@@ -18,6 +18,15 @@ def get_color_choice():
     print('  пурпурный')
     print('  розовый')
     color1 = input('Пожалуйста, введите первый цвет:')
+    if color1 == 'красный' or color1 == 'синий' or color1 == 'зеленый' or color1 == 'желтый' or color1 == 'пурпурный' or color1 == 'розовый':
+        color2 = input('Пожалуйста, введите второй цвет:')
+        if color2 == 'красный' or color2 == 'синий' or color2 == 'зеленый' or color2 == 'желтый' or color2 == 'пурпурный' or color2 == 'розовый':
+            return color1, color2
+        while color2 != 'красный' or color2 != 'синий' or color2 != 'зеленый' or color2 != 'желтый' or color2 != 'пурпурный' or color2 != 'розовый':
+            print("'" + color2 + "'", 'не является верным значением.')
+            color2 = input('Пожалуйста, введите цвет:')
+            if color2 == 'красный' or color2 == 'синий' or color2 == 'зеленый' or color2 == 'желтый' or color2 == 'пурпурный' or color2 == 'розовый':
+                return color1, color2
     while color1 != 'красный' or color1 != 'синий' or color1 != 'зеленый' or color1 != 'желтый' or color1 != 'пурпурный' or color1 != 'розовый':
         print("'" + color1 + "'", 'не является верным значением.')
         color1 = input('Пожалуйста, введите цвет:')
@@ -30,6 +39,7 @@ def get_color_choice():
                 color2 = input('Пожалуйста, введите цвет:')
                 if color2 == 'красный' or color2 == 'синий' or color2 == 'зеленый' or color2 == 'желтый' or color2 == 'пурпурный' or color2 == 'розовый':
                     return color1, color2
+
 
 # TO-DO(Victoria): function of number of the hexagons, need to check whether data entry is correct, 4 <= N <= 20
 def get_num_hexagons(N):
