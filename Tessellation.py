b0848b1c6a6ed68baf9a2d65f41d8ad1508a3b84
Victoria <vmoix001@mail.ru>
2020-03-20 15:22:49 +0700

    the commitments

--- Tessellation.py
+++ Tessellation.py
@@ -1,6 +1,23 @@
-"""Case-study #5 Тесселяция
-Разработчики:
-Беседина Д., Сицков М., Моисеенко В.
+# Case-study #5 "Tessellation"
+# Developers: Besedina D. (%)
+#             Setskov M. (%)
+#             Moiseenko V.(%)
 
-"""
 import turtle
+
+turtle = turtle.Turtle()
+
+# TO-DO(Diana): function of choosing two colors from at least six suggested ones, reentry if the color's name is wrong
+def get_color_choice():
+    pass
+
+# TO-DO(Victoria): function of number of the hexagons, need to check whether data entry is correct, 4 <= N <= 20
+def get_num_hexagons(N):
+    pass
+
+# TO-DO(Maxim): to draw the lines of hexagons
+# TO-DO(Victoria): to draw the hexagon + code review
+def draw_hexagon(x, y, side_len, color):
+    pass
+
+turtle.done()
