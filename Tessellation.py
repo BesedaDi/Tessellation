5b62da217a5038aeb644f29ec41ea0b87117b952
BesedaDi <dianabesedina223@gmail.com>
2020-03-20 16:21:30 +0700

    start

--- Tessellation.py
+++ Tessellation.py
@@ -10,7 +10,26 @@ turtle = turtle.Turtle()
 
 # TO-DO(Diana): function of choosing two colors from at least six suggested ones, reentry if the color's name is wrong
 def get_color_choice():
-    pass
+    print('Допустимые цвета заливки:')
+    print('  красный')
+    print('  синий')
+    print('  зеленый')
+    print('  желтый')
+    print('  пурпурный')
+    print('  розовый')
+    color1 = input('Пожалуйста, введите первый цвет:')
+    while color1 != 'красный' or color1 != 'синий' or color1 != 'зеленый' or color1 != 'желтый' or color1 != 'пурпурный' or color1 != 'розовый':
+        print("'" + color1 + "'", 'не является верным значением.')
+        color1 = input('Пожалуйста, введите цвет:')
+        if color1 == 'красный' or color1 == 'синий' or color1 == 'зеленый' or color1 == 'желтый' or color1 == 'пурпурный' or color1 == 'розовый':
+            color2 = input('Пожалуйста, введите второй цвет:')
+            if color2 == 'красный' or color2 == 'синий' or color2 == 'зеленый' or color2 == 'желтый' or color2 == 'пурпурный' or color2 == 'розовый':
+                return color1, color2
+            while color2 != 'красный' or color2 != 'синий' or color2 != 'зеленый' or color2 != 'желтый' or color2 != 'пурпурный' or color2 != 'розовый':
+                print("'" + color2 + "'", 'не является верным значением.')
+                color2 = input('Пожалуйста, введите цвет:')
+                if color2 == 'красный' or color2 == 'синий' or color2 == 'зеленый' or color2 == 'желтый' or color2 == 'пурпурный' or color2 == 'розовый':
+                    return color1, color2
 
 # TO-DO(Victoria): function of number of the hexagons, need to check whether data entry is correct, 4 <= N <= 20
 def get_num_hexagons(N):
